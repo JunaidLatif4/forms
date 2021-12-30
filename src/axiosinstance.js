c421bca9ed2c3ddd53f057e2207218e7be099904
Junaid Latif <junaidlatif004@gmail.com>
2021-12-31 01:34:09 +0500

    Proxy Added

--- src/axiosinstance.js
+++ src/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 let axiosInstance = axios.create({
-    baseURL:"http://5.182.33.224/"
+    baseURL: "https://cors-handle.herokuapp.com/http://5.182.33.224/"
 })
 
 export default axiosInstance
